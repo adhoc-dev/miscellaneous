807f3be58dcfddec908a47ba44661fab10be90a3
Matias Peralta <matias_peralta1@hotmail.com>
2025-01-23 10:53:43 -0300

    [ADD] mail_ux: new module with new features

---
+++ mail_ux/models/mail_compose_message.py
@@ -0,0 +1,30 @@
+from odoo import models, fields, api
+from datetime import datetime, timedelta
+from odoo.exceptions import UserError
+
+class MailComposeMessage(models.TransientModel):
+    _inherit = 'mail.compose.message'
+
+    def _action_send_mail(self, auto_commit=False):
+        """
+        Heredado para incluir un retraso de 30 segundos al enviar mensajes.
+        """
+        scheduled_date = datetime.now() + timedelta(seconds=30)
+        result_mails_su, result_messages = super(MailComposeMessage, self)._action_send_mail(auto_commit=auto_commit)
+        if self.composition_mode != 'mass_mail':
+            for wizard in self:
+                res_ids = wizard._evaluate_res_ids()
+                for res_id in res_ids:
+                    self.env['mail.scheduled.message'].create({
+                        'attachment_ids': [(6, 0, wizard.attachment_ids.ids)],
+                        'author_id': self.env.user.partner_id.id,
+                        'body': wizard.body,
+                        'model': wizard.model,
+                        'res_id': res_id,
+                        'partner_ids': [(6, 0, wizard.partner_ids.ids)],
+                        'scheduled_date': scheduled_date,
+                        'subject': wizard.subject,
+                        'notification_parameters': '{}',
+                    })
+
+        return result_mails_su, result_messages
